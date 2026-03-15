55e352c2b6a1d0e2aa0f9e5c9b8d906f5dd4be31
livorish <hoksini@gmail.com>
2026-03-15 21:44:39 +0300

    spliting app into the modules

--- index.js
+++ index.js
@@ -1,31 +1,14 @@
-const quotes = [
-  {
-    quote:
-      'Двумя самыми важными днями в твоей жизни являются день, когда ты появился на свет, и день, когда понял, зачем.',
-    author: 'Марк Твен',
-  },
-  {
-    quote: 'Я мыслю, следовательно, существую.',
-    author: 'Рене Декарт',
-  },
-  {
-    quote: 'Победа — это еще не все, все — это постоянное желание побеждать.',
-    author: 'Винс Ломбарди',
-  },
-  {
-    quote:
-      'Если вы хотите жить счастливой жизнью, привязывайте её к целям, а не к людям или вещам.',
-    author: 'Альберт Эйнштейн',
-  },
-];
+import allQuotes from './quotes.js';
+
+console.log(allQuotes);
 
 const quoteElement = document.getElementById('quote');
 const generateBtn = document.getElementById('generate-btn');
 const quoteAuthorElement = document.getElementById('quote-author');
 
 function generateRandomQuote() {
-  const randomIndex = Math.floor(Math.random() * quotes.length);
-  const randomQuote = quotes[randomIndex];
+  const randomIndex = Math.floor(Math.random() * allQuotes.length);
+  const randomQuote = allQuotes[randomIndex];
   const { quote, author } = randomQuote;
   quoteElement.textContent = quote;
   quoteAuthorElement.textContent = author;
