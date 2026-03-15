863049bcf89cf4f6f51203bc63873d3ec790d96e
livorish <hoksini@gmail.com>
2026-03-15 11:20:15 +0300

    Show quote and author separately and modidied quotes array of string to array of objects

--- index.js
+++ index.js
@@ -1,16 +1,34 @@
 const quotes = [
-  'This only way',
-  'Innovation distinguishes',
-  "Your time is limited, so don't waste it living someone else's life. - Steve Jobs",
+  {
+    quote:
+      'Двумя самыми важными днями в твоей жизни являются день, когда ты появился на свет, и день, когда понял, зачем.',
+    author: 'Марк Твен',
+  },
+  {
+    quote: 'Я мыслю, следовательно, существую.',
+    author: 'Рене Декарт',
+  },
+  {
+    quote: 'Победа — это еще не все, все — это постоянное желание побеждать.',
+    author: 'Винс Ломбарди',
+  },
+  {
+    quote:
+      'Если вы хотите жить счастливой жизнью, привязывайте её к целям, а не к людям или вещам.',
+    author: 'Альберт Эйнштейн',
+  },
 ];
 
 const quoteElement = document.getElementById('quote');
 const generateBtn = document.getElementById('generate-btn');
+const quoteAuthorElement = document.getElementById('quote-author');
 
 function generateRandomQuote() {
   const randomIndex = Math.floor(Math.random() * quotes.length);
   const randomQuote = quotes[randomIndex];
-  quoteElement.textContent = randomQuote;
+  const { quote, author } = randomQuote;
+  quoteElement.textContent = quote;
+  quoteAuthorElement.textContent = author;
 }
 
 generateBtn.addEventListener('click', generateRandomQuote);
